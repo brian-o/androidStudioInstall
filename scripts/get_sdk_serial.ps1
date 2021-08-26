4c650f8f06fbfb67515700c467bd25f0be3b3f9b
brian-o <brianodellk@gmail.com>
2021-08-25 23:54:42 -0700

    Sdk build chain changes
    Small sdk builds in serial now.
    Added fullsdk config
    Added github actions for small and full builds

---
+++ scripts/get_sdk_serial.ps1
@@ -0,0 +1,64 @@
+param (
+  $destination,
+  $javaHome,
+  $pathToLicenses,
+  $sdkConfigPath
+)
+if ($pathToLicenses -notmatch '\\$') {
+  $pathToLicenses += '\'
+}
+if ($destination -notmatch '\\$') {
+  $destination += '\'
+}
+$startingDir = Get-Location
+Set-Location $destination
+
+$sdkRoot = "$($destination)Sdk\"
+Write-Output "Writing SDK to $sdkRoot"
+
+function Get-SdkComponent {
+  param (
+    $sdkManager,
+    [string]$componentName,
+    [string]$sdkRoot
+  )
+  Write-Output "y"| & "$sdkManager" --sdk_root=$sdkRoot $componentName
+}
+
+$env:JAVA_HOME = $javaHome
+Write-Output "JavaHome set to $env:JAVA_HOME"
+Write-Output "This will not persist after this powershell session ends"
+
+$command_line_tools = "commandlinetools-win-7302050_latest"
+$command_line_tools_url = "https://dl.google.com/android/repository/$($command_line_tools).zip"
+$command_line_tools_filename = $destination + "$($command_line_tools).zip"
+$command_line_tools_folder = $destination + "$($command_line_tools)\"
+
+# Now download the command line tools so we can download the sdk
+# This command should block until it is complete
+
+Write-Output "Downloading $command_line_tools_url"
+$web_client = New-Object System.Net.WebClient
+$web_client.DownloadFile($command_line_tools_url, $command_line_tools_filename)
+Write-Output "Unzipping $command_line_tools_filename"
+Expand-Archive -Path $command_line_tools_filename -DestinationPath $command_line_tools_folder -Force
+
+# will manually need to accept the licenses
+$sdkman = $command_line_tools_folder + "cmdline-tools\bin\sdkmanager.bat"
+
+Write-Output "y"| & "$sdkman" --sdk_root=$sdkRoot --update
+Write-Output "y"| & "$sdkman" --sdk_root=$sdkRoot --licenses 
+
+robocopy "$($pathToLicenses)" "$($sdkRoot)licenses\" /e
+Write-Output "y"| & "$sdkman" --sdk_root=$sdkRoot --licenses
+
+$components = Get-Content -Path "$sdkConfigPath"
+ForEach ($component in $components) {
+  Get-SdkComponent -sdkManager $sdkMan -sdkRoot $sdkRoot -componentName $component
+}
+
+robocopy "$($pathToLicenses)" "$($destination)licenses\" /e
+Write-Output "y"| & "$sdkman" --sdk_root=$sdkRoot --licenses
+      
+
+Set-Location $startingDir
